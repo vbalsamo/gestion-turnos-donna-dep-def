11eb82c824cbba0ec9bbc02141da9b7c9c54bffd
David Achas <AchasDavid@users.noreply.github.com>
2022-12-05 17:04:12 -0300

    Update AppServiceProvider.php
    Añadida la validación de letras y espacios en blanco como alpha_spaces

--- Grupo3LaravelProject/app/Providers/AppServiceProvider.php
+++ Grupo3LaravelProject/app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
     {
         $tratamientos = DB::select("SELECT * FROM tratamiento");
         View::share('tratamientos_global', $tratamientos);
+        
+        Validator::extend('alpha_spaces', function ($attribute, $value){
+            return preg_match('/^[\pL\s]+$/u', $value);
+        });
     }
 }
