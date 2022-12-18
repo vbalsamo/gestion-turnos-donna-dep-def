1442cb2495337b7fa71388e2247ea9ca4b8fc2e4
David Achas <AchasDavid@users.noreply.github.com>
2022-12-17 22:59:07 -0300

    Corregida ruta de Turnos

--- Grupo3LaravelProject/resources/views/base.blade.php
+++ Grupo3LaravelProject/resources/views/base.blade.php
@@ -32,7 +32,7 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="{{ route('turnos.index') }}">Turnos<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="{{ route('calendarioSelect.index') }}">Turnos<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item active">
                     <a class="nav-link" href="{{ route('locaciones.index') }}">Sucursales</a>
