c6eff67c9d700ec1788d77ef3f73286952772a66
David Achas <AchasDavid@users.noreply.github.com>
2022-12-17 23:14:40 -0300

    Añadida dependencia actualizada de Bootstrap

--- Grupo3LaravelProject/resources/views/base.blade.php
+++ Grupo3LaravelProject/resources/views/base.blade.php
@@ -15,6 +15,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
             integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
             crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/base.css') }}">
     @yield('contenido_header')
     <title>Donna Depilación Definitiva</title>
