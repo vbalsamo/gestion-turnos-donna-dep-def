6a827c0b81f3fe9a53f3fc9fdf2f38bdaaed8e24
martin-vainer <vainermartin@gmail.com>
2022-12-19 16:41:50 -0300

    vistas menu cliente-admin

--- Grupo3LaravelProject/resources/views/base.blade.php
+++ Grupo3LaravelProject/resources/views/base.blade.php
@@ -5,15 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-            crossorigin="anonymous"></script>
+            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
             integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
             crossorigin="anonymous"></script>
     @yield('contenido_header')
     <link rel="stylesheet" type="text/css" href="{{ asset('css/base.css') }}">
+
+    @yield('contenido_header')
     <title>Donna Depilación Definitiva</title>
 </head>
 
@@ -82,10 +81,12 @@
                         </li>
                     </ul>
                 @else
-                    <form action="{{ route('login.index') }}">
-                        <button id="btnIngresar" type="submit" class="btn btn-primary shadow">Iniciar Sesión
-                        </button>
-                    </form>
+                        <li class="nav-item dropdown active" style="position: relative; left: 190%;">
+                            <form action="{{ route('login.index') }}">
+                                <button id="btnIngresar" type="submit" class="btn btn-primary shadow">Iniciar Sesión
+                                </button>
+                            </form>
+                        </li>
                 @endif
             </div>
         </div>
