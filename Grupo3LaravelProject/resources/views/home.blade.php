f4d677cc5a93c2d387cf0273525e55e99fa77085
martin-vainer <vainermartin@gmail.com>
2022-12-20 01:14:56 -0300

    arreglo de carrusel

--- Grupo3LaravelProject/resources/views/home.blade.php
+++ Grupo3LaravelProject/resources/views/home.blade.php
@@ -6,32 +6,30 @@
 
 <!-- CAROUSEL -->
 
-<div id="carouselHome" class="carousel slide" data-ride="carousel">
+<div id="carouselExampleIndicators" class="carousel slide pause" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     
-    <ol class="carousel-indicators">
-        <li data-target="#carouselHome" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselHome" data-slide-to="1"></li>
-    </ol>
-
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="{{ asset('verano.png') }}" alt="..." class="d-block w-100">
-        </div>
-        <div class="carousel-item">
-            <img src="{{ asset('promocion_navidad.png') }}" alt="..." class="d-block w-100">
-        </div>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="{{ asset('verano.png') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="10000">
+      <img src="{{ asset('promocion_navidad.png') }}" class="d-block w-100" alt="...">
     </div>
     
-    <a href="#carouselHome" class="carousel-control-prev" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden shadow">Anterior</span>
-    </a>
-    <a href="#carouselHome" class="carousel-control-next" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden shadow">Siguiente</span>
-    </a>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Anterior</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Siguiente</span>
+  </button>
 </div>
-
 <!-- FIN CAROUSEL -->
 
 <!-- DESCRIPCION EN 3 COL -->
