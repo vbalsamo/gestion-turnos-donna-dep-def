d1e2e1e85660e41f4abc9e04f0cc1fa2e7e9db1e
valenhz <balvalentino@gmail.com>
2022-12-25 20:45:00 -0300

    load assets on production env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
@@ -28,6 +29,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') == 'production') {
+            URL::forceScheme('https');
+        }
+
         View::share('tratamientos_global', DB::select("SELECT * FROM tratamiento WHERE activo = 1"));
 
         View::share('locaciones_global', DB::select("SELECT * FROM locacion WHERE activo = 1"));
@@ -36,23 +41,23 @@ class AppServiceProvider extends ServiceProvider
 
         //View::share('meses_global', DB::select("SELECT * FROM mes WHERE activo = 1"));
 
-        Validator::extend('alpha_spaces', function ($attribute, $value){
+        Validator::extend('alpha_spaces', function ($attribute, $value) {
             return preg_match('/^[\pL\s]+$/u', $value);
         });
 
-        Gate::define('adminAuth', function ($usuario){
+        Gate::define('adminAuth', function ($usuario) {
             $id = Auth::id();
             $rol = DB::selectOne("SELECT * FROM cliente WHERE id={$id}")->rol;
             return $rol == "admin";
             //return $usuario->getRol() == 'admin';
-        }) ;
+        });
 
-        Gate::define('clienteAuth', function ($usuario){
+        Gate::define('clienteAuth', function ($usuario) {
             $id = $usuario->id;
             $rol = DB::selectOne("SELECT rol FROM cliente WHERE id={$id}")->rol;
             //dd($id, $rol);
             return $rol == 'cliente';
             //return $usuario->getRol() == 'admin';
-        }) ;
+        });
     }
 }
